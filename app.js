5820f7ed1e3746e958070237f72e6c2ac77066f5
Oblivion Plain <45317380+oblivionplain@users.noreply.github.com>
2020-12-11 19:55:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
 let a = new Image();
-image.src = ships[0].image;
+a.src = ships[0].image;
 
 ctx.drawImage(a, 100, 100);
